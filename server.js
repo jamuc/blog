b4f8ab4ea9c6898d94934cb4cfe48c1e946e1d82
Jason Franklin <jason.b.franklin@gmail.com>
2016-08-08 19:16:25 +0200

    Serve index.html and stylesheets

--- server.js
+++ server.js
@@ -7,9 +7,29 @@ var path = require('path');
 var mime = require('mime');
 
 app.get("/", function(request, response) {
-  response.writeHead(200, { "Content-Type" : "text/plain" });
-  response.write("Hello world");
-  response.end();
+  streamFile(response, "./public/index.html");
 });
+
+app.get("/:filename", function(request, response) {
+  streamFile(response, "./public/" + request.params.filename);
+});
+
+app.get("/stylesheets/:filename", function(request, response) {
+  streamFile(response, "./public/stylesheets/" + request.params.filename);
+});
+
+function streamFile(response, filePath) {
+  fs.exists(filePath, function(exist) {
+    if(exist) {
+      response.writeHead(200, { "Content-Type" : mime.lookup(path.basename(filePath)) });
+      var stream  = fs.createReadStream(filePath);
+      stream.pipe(response);
+    } else {
+      response.writeHead(404, { "Content-Type" : 'text/plain' });
+      response.write("404: File not found");
+      response.end();
+    }
+  });
+}
 
 server.listen(process.env.PORT || 3000);
